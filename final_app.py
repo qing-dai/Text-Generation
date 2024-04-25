e79853a6de65fbc18e45a194d204b4b530a9a47e
qing-dai <qing.dai@uzh.ch>
2024-04-25 20:12:55 +0200

    Add force_word option for output

--- final_app.py
+++ final_app.py
@@ -1,39 +1,59 @@
 import gradio as gr
 
-from transformers import pipeline, GenerationConfig
+from transformers import pipeline, GenerationConfig,  PhrasalConstraint
 
 # Create a text generation pipeline
 generator = pipeline("text-generation", model="gpt2")
 config = GenerationConfig.from_pretrained("gpt2")
 
-
-# config = GenerationConfig.from_pretrained("gpt2")
-
 # The default config of GPT-2 is greedy decoding
 
-# Generate text
-# generator("Once upon a time, ", max_new_tokens=20, num_beams = 5)
 
-def text_generate(prompt, temperature, maximum_length, top_p):
+def text_generate(prompt, temperature, maximum_length, top_p, num_beams=3, force_word=None):
     # config max_length first
     config.max_new_tokens = maximum_length
-    # depending on temp and top_p, we decide whether to use greedy_search or not
-    if temperature == 0 or top_p == 0:
+    # Add force_word option for output
+    if force_word:
         config.do_sample = False
+        force_word_id = generator.tokenizer(force_word, add_special_tokens=False).input_ids
+        constraints = [
+            PhrasalConstraint(force_word_id),
+        ]
+        generated_text = generator(prompt, num_beams=num_beams if num_beams > 1 else 3,
+                                   generation_config=config,
+                                   constraints=constraints)
+        print(f"I am using force model")
+    # depending on temp and top_p, we decide whether to use greedy_search or not
+    elif temperature == 0 or top_p == 0:
+        config.do_sample = False
+        generated_text = generator(prompt,
+                                   generation_config=config)
+        print("I am using 0 model")
     else:
         config.do_sample = True
         config.temperature = temperature
         config.top_p = top_p
-    generated_text = generator(prompt,
-                               generation_config=config)
+        generated_text = generator(prompt,
+                                   generation_config=config)
+        print("I am using top_p model")
+
+    print(config.do_sample)
+
     return generated_text[0]["generated_text"]
+
 
 
 # use actual class for gr.Textbox to have access to much more customizablility
 
 demo = gr.Interface(
     fn=text_generate,
-    inputs=[gr.Textbox(label="Prompt", value="Type in whatever you want to start with", lines=5),
+    # Allow users to select pre-defined prompts from dropdowns
+    inputs=[gr.Dropdown(['Today is Christmas, ',
+                        "Type in whatever you want to start with",
+                        "Let's go party, "],
+                        label="Prompt",
+                       value="Type in whatever you want to start with"
+                       ),
             gr.Slider(label="Temperature", value=1, minimum=0, maximum=2,
                       info="Adjusts randomness in text generation; lower values produce "
                            "more predictable text, higher values encourage creativity and variety."),
